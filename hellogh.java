340dc124ac40e3fc0ae41ba2898e30d56ba2c0fb
Gabe Jimenez <jimenega@gmail.com>
2020-08-19 10:14:55 -0400

    Simple changes

--- hellogh.java
+++ hellogh.java
@@ -1,8 +1,5 @@
-/**
- * hellogh.java
- */
 public class hellogh {
-    public static void hellogh( String[] args ) {
+    public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("Good Bye GitHub");
     }
