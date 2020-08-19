443e8984bae2b1943aa54f7cd08fd09350dc1f9b
Gabe Jimenez <jimenega@gmail.com>
2020-08-19 09:02:14 -0400

    First commit

---
+++ hellogh.java
@@ -0,0 +1,9 @@
+/**
+ * hellogh.java
+ */
+public class hellogh {
+    public static void hellogh( String[] args ) {
+        System.out.println("Hello GitHub");
+        System.out.println("Good Bye GitHub");
+    }
+} 
